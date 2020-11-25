85da292f2cb21897f5b826c2a090fdd3ddd65fd2
Avenger <Avenger@CAPTAINVJ>
2020-11-25 11:59:27 +0530

    New changes made from eclipse by QA1

--- GoogleDriveTest/src/test/java/com/facebook/pages/GitTestPage.java
+++ GoogleDriveTest/src/test/java/com/facebook/pages/GitTestPage.java
@@ -5,6 +5,7 @@ public class GitTestPage {
 	public void GitTest1()
 	{
 		System.out.println("Changes made by Tester(Github2)");
+		System.out.println("i have made new changes from eclipse");
 	// TODO Auto-generated method stub
 
 }
